81e419e236f9f17e33e1ae7eb0ceb1b442b07ec9
suvernet <74303408+suvernet@users.noreply.github.com>
2026-08-20 22:27:45 +0300

    Update tag__generator.js
    обновили скрипт

--- tag__generator.js
+++ tag__generator.js
@@ -2,21 +2,42 @@
 const textInput = document.getElementById('text');
 const urlInput = document.getElementById('url');
 const generateBtn = document.getElementById('generate');
+
+const imgUrlInput = document.getElementById('imgUrl');
+const altTextInput = document.getElementById('altText');
+const widthInput = document.getElementById('width');
+const heightInput = document.getElementById('height');
+const imgBtn = document.getElementById('IMG');
+
 const resultDiv = document.getElementById('result');
 
-// Функция генерации тега
-function generateTag() {
+// Функция генерации тега ссылки
+function generateLinkTag() {
     const text = textInput.value;
     const url = urlInput.value;
 
     // Формируем HTML-код (добавляем target="_blank" для открытия в новой вкладке)
     const htmlTag = `<a href="${url}" target="_blank">${text}</a>`;
-    const imgTag = `<img src="${imgUrl}" alt="${altText}" width="${width}" height="${height}" />`;
-
 
     // Выводим результат
     resultDiv.innerHTML = htmlTag;
 }
 
-// Привязываем функцию к нажатию кнопки
-generateBtn.addEventListener('click', generateTag);
+// Функция генерации тега картинки
+function generateImgTag() {
+    const imgUrl = imgUrlInput.value;
+    const altText = altTextInput.value;
+    const width = widthInput.value;
+    const height = heightInput.value;
+
+    // Формируем HTML-код
+    const imgTag = `<img src="${imgUrl}" alt="${altText}" width="${width}" height="${height}" />`;
+
+    // Выводим результат
+    resultDiv.innerHTML = imgTag;
+}
+
+// Привязываем функции к нажатию кнопок
+generateBtn.addEventListener('click', generateLinkTag);
+imgBtn.addEventListener('click', generateImgTag);
+
